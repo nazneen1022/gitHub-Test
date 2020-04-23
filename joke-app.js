15d41e6459dfe04a7b24c4cf4715e148070782fc
nazneen1022 <nazneen1022@gmail.com>
2020-04-23 14:48:33 +0200

    added age param

--- joke-app.js
+++ joke-app.js
@@ -5,10 +5,8 @@ const app = importExpress(); // create a server
 app.listen(3000,()=>{console.log(`Listening to port 3000`)}); // listening to the port
 
 // GET endpoint with arg1: route and arg2: request and response
-app.get('/userName?&age&birthMonth',(request,response)=>{
-    const paramAge = getUrlParam("age","age");
-    response.send(`Age : 
-    // <h5>${paramAge}</h5>
-    // <h1>${request.params.birthMonth}<h1>`);
-
+app.get("/Jokes/:age", (request,response) => {
+    response.send(`Hello World!!
+    Age : ${request.params.age}`);
+    
 }); 
