16139a631025a38caf55d5ad1a3429eec4dc4bf1
nazneen1022 <nazneen1022@gmail.com>
2020-04-24 11:38:55 +0200

    Added Html

--- joke-app.js
+++ joke-app.js
@@ -1,12 +1,63 @@
 // Joke app server
 const importExpress = require('express');
+
+// import the data
+const jokes = require('./jokes')
+console.log(jokes);
+
+const min =1;
+const max = parseInt(Object.keys(jokes).length);
+
 const app = importExpress(); // create a server
 
 app.listen(3000,()=>{console.log(`Listening to port 3000`)}); // listening to the port
 
+
+app.get('/student/:name', (request, response) =>{
+    
+})
+
+
 // GET endpoint with arg1: route and arg2: request and response
-app.get("/Jokes/:age", (request,response) => {
-    response.send(`Hello World!!
-    Age : ${request.params.age}`);
+app.get("/Jokes/:age/:birthMonth", (request,response) => {
+    const age = parseInt(request.params.age);
+    const month = parseInt(request.params.birthMonth);
+    const randomNum = Math.floor(Math.random() * (+max - +min)) + +min;
+
+    if (request.params.age === "" || request.params.birthMonth === ""){
+        res =`Your input URL must have two params (age and birth month)
+        URL: localhost:3000/jokes/:age/:birthMonth
+
+        Sample URL should look like
+        localhost:3000/jokes/26/11`;
+    }
+
+    if (isNaN(age) || age > 110 || age < 3){
+        res =`Your input ion the URL for age is invalid
+        URL: localhost:3000/jokes/:age/:birthMonth
+
+        Sample URL should look like
+        localhost:3000/jokes/26/11`;
+    }
+
+    if (isNaN(month) || month < 1 || month > 12){
+        res =`Your input in the URL for month of birth is invalid
+        URL: localhost:3000/jokes/:age/:birthMonth
+
+        Sample URL should look like
+        localhost:3000/jokes/26/11`;
+    }
+
+const chooseJoke = age < 18 ? jokes["childJokes"][month]: (age >=18 && age <70)?jokes["sarcasmJokes"][month]:jokes["oldJokes"][month];
+
+const res =`<html>
+<h4>Your Joke for today!!<h4>
+<p>${chooseJoke}</p>
+  </html>`
+    
+// const pickImage = jokes.find((joke)=>{
+//     return joke === randomNum});
+
+    response.send(res);
     
 }); 
---
+++ jokes.js
@@ -0,0 +1,32 @@
+// jokes object
+module.exports = {
+    "childJokes": {
+        1: `How do you know when a bike is thinking? A: You can see its wheels turning.`,
+        2: `What game does the sky love to play? A: Twister.`,
+        3: `Why do we never tell jokes about pizza? A: They’re too cheesy`,
+        4: `What do you do if someone rolls their eyes at you? A: Roll them back`,
+        5: `Do you know how many famous men and women were born on your birthday? A: None, only babies`,
+        6: `Why didn’t the lamp sink? A: It was too light`,
+        7: `Where were pencils invented? A: PENCIL-vania`,
+        8: `Where do cows go for entertainment?A: Moo-vies`,
+        9: `What gets wetter the more it dries?A: A towel.`,
+        10: `Where do hamburgers go dancing?A: A meatball`,
+        11: `How do elves learn how to spell?A: They study the elf-abet`,
+        12: `Why can’t your hand be 12 inches long?A: Because then it would be a foot.`
+    },
+    "sarcasmJokes": {
+        1: `How does a computer get drunk? A: It takes screenshots`,
+        2: `Why did the computer show up at work late? A: It is a hard-drive`,
+        3: `What do you call an iPhone that isn't kidding around? A: Dead Siri-ous`,
+        4: `Why was the cell phone wearing glasses? A: It lost its contacts`,
+        5: `How do you make a pirate furious? A: Take away the p`,
+        6: `Why shouldn't you fall in love with a pastry chef? A: He'll dessert you.`,
+        7: `What has T in the beginning, T in the middle, and T at the end? A: A teapot`,
+        8: `Who's a dessert's favorite actor? A: Robert Brownie, Jr.`,
+        9: `What do you get from a pampered cow? A: Spoiled milk.`,
+        10: `Why did the two 4's skip lunch? A: They already 8 (ate)!`,
+        11: `What did 2 say to 4 after 2 beat him in a race? A: 2 Fast 4 U`,
+        12: `How does a tech guy drink coffee? A: He installs Java!`,
+    },
+
+};
